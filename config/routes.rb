ce044cdf40201851f0f766500bbda6116f09ad69
112259071+nakanosyouta@users.noreply.github.com <112259071+nakanosyouta@users.noreply.github.com>
2022-10-11 11:45:25 +0000

    naosi

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'books/new'
   root to: 'homes#top'
   post 'books' => 'books#create'
-  get 'books' => 'books#index'
+  get 'books' => 'books#index', as: 'index_book'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
